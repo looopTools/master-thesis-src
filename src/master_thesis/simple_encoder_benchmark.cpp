0465091f2b2f54b58a4a37a8f7e52a37a2ba4825
Lars Nielsen <lnc13.lars@gmail.com>
2018-03-26 12:55:59 +0200

    :edit: New threadpool approach

--- src/master_thesis/simple_encoder_benchmark.cpp
+++ src/master_thesis/simple_encoder_benchmark.cpp
@@ -1,10 +1,18 @@
+
+#include "encoder/simple_encoder.hpp"
+
 #include <kodo_rlnc/full_vector_codes.hpp>
+
+
 
 int main()
 {
     using rlnc_encoder = kodo_rlnc::full_vector_encoder;
     using rlnc_decoder = kodo_rlnc::full_vector_decoder;
     fifi::api::field field = fifi::api::field::binary8;
+
+    uint32_t symbols = 10;
+    uint32_t symbol_size = 40;
 
     // In the following we will make an encoder/decoder factory.
     // The factories are used to build actual encoders/decoders

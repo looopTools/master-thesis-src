ac722f57890720f8d54e71d67909ec4c323d061c
Lars Nielsen <lnc13.lars@gmail.com>
2018-03-31 21:28:47 +0200

    :new: added a complex encoder

---
+++ src/master_thesis/complex_encoder_benchmark.cpp
@@ -0,0 +1,26 @@
+#include "encoder/complex_encoder.hpp"
+
+#include <kodo_rlnc/full_vector_codes.hpp>
+
+
+#include <cstdint>
+#include <vector>
+
+int main()
+{
+    fifi::api::field field = fifi::api::field::binary8;
+
+    uint32_t symbols = 10;
+    uint32_t symbol_size = 40;
+    uint32_t threads = 2;
+
+    std::vector<uint8_t> data;
+    data.push_back(0u);
+
+    master_thesis::complex_encoder encoder(symbols, symbol_size, threads, field, data);
+    // In the following we will make an encoder/decoder factory.
+    // The factories are used to build actual encoders/decoders
+
+    return 0;
+
+}
